4a3ce8756ba10ca4aceaf1df2991b632c16d654a
Aradhya Tiwari <aradhya.prad@gmail.com>
2023-02-07 08:03:40 +0530

    notes form

--- src/routes/notes/+page.svelte
+++ src/routes/notes/+page.svelte
@@ -1,8 +1,38 @@
+<script>
+    import { each } from "svelte/internal";
+
+    let branches = [
+        {
+            name: "B.tech CSE",
+            branch: "btech_cs",
+            sem: 8,
+        },
+        {
+            name: "B.tech ME",
+            branch: "btech_me",
+            sem: 8,
+        },
+        {
+            name: "B.tech EE",
+            branch: "btech_ee",
+            sem: 8,
+        },
+    ];
+    let sems = {};
+</script>
+
 <div class="m-auto w-40 border-black border-2">
-    <select name="branch" id="" class="w-full border-black border-2">
-        <option value="btech_cs">B.Tech CSE</option>
-        <option value="bpharma">B.Pharma</option>
-        <option value="btech_ee">B.Tech EE</option>
-        <option value="btech_me">B.Tech ME</option>
+    <select
+        name="branch"
+        id=""
+        bind:value={sems}
+        class="w-full border-black border-2"
+    >
+        {#each branches as branch}
+            <option value={branch}>
+                {branch.name}
+            </option>
+        {/each}
     </select>
+    <input type="radio" name="1" id="" value="${sems}" />{sems.name || "null"}
 </div>
